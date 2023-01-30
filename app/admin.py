7f4e8003c2a60c309f2a0047271f044bf9e41af7
damianwastaken <damianatlass@gmx.de>
2023-01-30 21:27:11 +0100

    added models: Dialog, DialogMessage, GraphMessage

--- app/admin.py
+++ app/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
-from .models import UserInfo
+from .models import *
 
 
 admin.site.register(UserInfo)
+admin.site.register(Dialog)
+admin.site.register(DialogMessage)
+admin.site.register(GraphMessage)
 # Register your models here.
--- app/models.py
+++ app/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
+USER = "USER"
 
 class UserInfo(models.Model):
     user = models.OneToOneField(User, related_name="userinfo",  on_delete=models.CASCADE, null=True)
@@ -15,3 +16,29 @@ class UserInfo(models.Model):
     class Meta:
         verbose_name = "UserInfo"
         verbose_name_plural = "UserInfos"
+
+
+class GraphMessage(models.Model):
+    content = models.CharField(max_length=240, null=True)
+    author = models.CharField(max_length=25, null=True)
+    prev = models.ManyToManyField("self")
+    next = models.ManyToManyField("self")
+    is_start = models.BooleanField(null=False, default=False)
+    is_end = models.BooleanField(null=False, default=False)
+
+    def __str__(self):
+        return f"{self.author}: '{self.content}'"
+
+class Dialog(models.Model):
+    bot_type = models.CharField(max_length=25, null=True)
+    user = models.OneToOneField(User, related_name="dialog",  on_delete=models.CASCADE, null=True)
+
+    def __str__(self):
+        return f"todo"
+
+
+class DialogMessage(models.Model):
+    #TODO: number field that indecates order
+    date = models.DateTimeField(auto_now_add=True)
+    dialog = models.ForeignKey(Dialog, related_name="messages", on_delete=models.CASCADE)
+    graph_message = models.ForeignKey(GraphMessage, on_delete=models.CASCADE)
